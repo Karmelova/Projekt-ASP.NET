581b2259862d9e467e5c3b94b84495bc1834024c
Karmelova <kamila.weronika.marcinek@gmail.com>
2022-12-27 03:51:57 +0100

    - Create app - added first classes - installed sqlite with EF - Added swagger ui

---
+++ YourITMatch/YourITMatch/Models/CompanyAddressModel.cs
@@ -0,0 +1,12 @@
+﻿namespace YourITMatch.Models
+{
+    public class CompanyAddressModel
+    {
+        public int ID { get; set; }
+        public int CompanyID { get; set; }
+        public string PostCode { get; set; }
+        public string City { get; set; }
+        public string Voivodeship { get; set; }
+        public string Street { get; set; }
+    }
+}
---
+++ YourITMatch/YourITMatch/Models/CompanyModel.cs
@@ -0,0 +1,19 @@
+﻿namespace YourITMatch.Models
+{
+    public class CompanyModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Email { get; set; }
+        public string NIP { get; set; }
+        public string Regon { get; set; }
+        public string PostCode { get; set; }
+        public string City { get; set; }
+        public string Voivodeship { get; set; }
+        public string Street { get; set; }
+        public int CompanySize { get; set; }
+        public DateOnly CompanyEstablished { get; set; }
+        public string CompanyWebsite { get; set; }
+    }
+}
